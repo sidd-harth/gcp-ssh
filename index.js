081c487e1481787d360a887e246ecb98e9ec3435
Siddharth Barahalikar <barahalikar.siddharth@gmail.com>
2019-04-16 15:21:07 +0530

    ssh2

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 //host configuration with connection settings and commands
 var host = {
  server:        {     
@@ -18,3 +19,29 @@ var SSH2Shell = require ('ssh2shell'),
   }
 //Start the process
 SSH.connect(callback);
+
+*/
+
+
+var Client = require('ssh2').Client;
+ 
+var conn = new Client();
+conn.on('ready', function() {
+  console.log('Client :: ready');
+  conn.exec('uptime', function(err, stream) {
+    if (err) throw err;
+    stream.on('close', function(code, signal) {
+      console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
+      conn.end();
+    }).on('data', function(data) {
+      console.log('STDOUT: ' + data);
+    }).stderr.on('data', function(data) {
+      console.log('STDERR: ' + data);
+    });
+  });
+}).connect({
+  host: '35.244.5.62',
+  port: 22,
+  username: 'sid',
+  privateKey: '/privateKey.ppk'
+});
