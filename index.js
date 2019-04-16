89662d97a545045e40c1e4e635c8d08b846a73d1
Siddharth Barahalikar <barahalikar.siddharth@gmail.com>
2019-04-16 15:14:26 +0530

    pp

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var host = {
   host: '35.244.5.62',
   port: 22,
   userName: 'sid',
-  privateKey: 'ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAksObRkXqYugpEgDk2MNCwBSo8/KagWVsgdE3TfkFC0cglkQl+xO3rEvX+NqejumBkrbozz1brAO+7vEQDbKF0hvz9WGDaz93GKxQa9MdDIytdYRundY0uA8PBoTiWG06KZvGFSOR9CzRT/Mi3P8fOktr8u2p7/w5sIdZ9ksCD1k2GdI3H8sYTv2nRRi8QFZTf7g5F5ZP6CLEtv+J/iCFdaypFci4K+HqKsv6bQGJ2GDDdzKefc+bOrQZi2Btv0FSieI5UJMSiKxoThWMXruopyamJM/A2UsTPuvsFh+RnGNQA0ea3r8CP+c55QPunTL8Cu0rZpakA8R3JUSIiFXBNw== sid',
+  privateKey: '/privateKey.ppk',
   },
  commands:      [ "echo $(pwd)", "sudo mkdir abcdeer" ]
 };
