4784ba671045924c256869541e44dc0e8e3251ee
Siddharth Barahalikar <barahalikar.siddharth@gmail.com>
2019-04-16 14:48:46 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+//host configuration with connection settings and commands
+var host = {
+ server:        {     
+  host: '35.244.5.62',
+  port: 22,
+  userName: 'sid',
+  privateKey: require('fs').readFileSync('/key/privateKey.ppk'),
+  },
+ commands:      [ "echo $(pwd)", "mkdir abcdeer" ]
+};
+ 
+var SSH2Shell = require ('ssh2shell'),
+  //Create a new instance passing in the host object
+  SSH = new SSH2Shell(host),
+  //Use a callback function to process the full session text
+  callback = function(sessionText){
+    console.log(sessionText)
+  }
+//Start the process
+SSH.connect(callback);
